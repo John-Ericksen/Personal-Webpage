c140d3e250e857e88068b9907e9c0d91b47c7c61
John-Ericksen <105228679+John-Ericksen@users.noreply.github.com>
2024-03-02 23:11:24 -0500

    complete backround

---
+++ Personal-Webpage/src/components/Backround/Backround.tsx
@@ -0,0 +1,11 @@
+import "./Backround.scss"
+
+export default function Backround() {
+  return(
+    <>
+      <div className="gradient"/>
+      <div className="border"/>
+    </>
+    
+  )
+}
---
+++ Personal-Webpage/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div className="header">
+      <h1>John Ericksen</h1>
+      <h2>Software Engineer and Artist</h2>
+    </div>
+    
+  )
+}
