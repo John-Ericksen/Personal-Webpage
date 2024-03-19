369a2b97b175177b4c3acdfd51577e2d80768496
John-Ericksen <105228679+John-Ericksen@users.noreply.github.com>
2024-03-19 19:19:45 -0400

    make experience section template

--- Personal-Webpage/src/components/Experience/Experience.tsx
+++ Personal-Webpage/src/components/Experience/Experience.tsx
@@ -2,9 +2,21 @@ import "./Experience.scss"
 
 export default function Experience(props:any) {
 
+    function ExperienceCard(props: any) {
+        return(
+            <div>
+                <div className="experience-header">
+                    <h3>{props.experienceTitle}</h3>
+                    <img src={props.experienceImage} alt="experience image" />
+                </div>
+                <p>{props.experienceText}</p>
+            </div>
+        )
+    }
+
     return(
         <div className={`experience ${props.sectionArray[2] ? "active" : "inactive"}`}>
-            <p>TESTING</p>
+            <ExperienceCard experienceTitle="Test Experience" experienceText="this is a demo experience" />
         </div>
     )
 }
