5d47998d7073022da7a14fcc3afec25d83903fb4
John Ericksen <john.paul.ericksen@gmail.com>
2024-03-21 18:24:01 -0400

    continue dev work on experience section

--- Personal-Webpage/src/components/Experience/Experience.tsx
+++ Personal-Webpage/src/components/Experience/Experience.tsx
@@ -1,22 +1,27 @@
 import "./Experience.scss"
+import "./ExperienceData"
+import ExperienceData from "./ExperienceData"
 
 export default function Experience(props:any) {
 
     function ExperienceCard(props: any) {
         return(
-            <div>
+            <div className="experience-card">
                 <div className="experience-header">
-                    <h3>{props.experienceTitle}</h3>
                     <img src={props.experienceImage} alt="experience image" />
+                    <h3>{props.experienceTitle}</h3>
                 </div>
+                <h3>{props.subtitle}</h3>
                 <p>{props.experienceText}</p>
             </div>
         )
     }
-
+    
+    const experienceCardList = ExperienceData.map((element)=><ExperienceCard key={crypto.randomUUID()} experienceTitle={element.title} experienceImage={element.image} experienceText={element.text} subtitle={element.subtitle}/>)
+    
     return(
         <div className={`experience ${props.sectionArray[2] ? "active" : "inactive"}`}>
-            <ExperienceCard experienceTitle="Test Experience" experienceText="this is a demo experience" />
+            {experienceCardList}
         </div>
     )
 }
---
+++ Personal-Webpage/src/components/Experience/ExperienceData.tsx
@@ -0,0 +1,17 @@
+const ExperienceData = [
+  {
+    title: "Amazon Web Services",
+    subtitle: "Software Engineering Intern",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s",
+    text: "Interned at Amazon, AWS branch. Designed and coded web pages along with improving accessibility features for blind and visually impaired users.",
+  },
+  {
+    title: "QuattronKids",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s",
+    text: "Interned at Amazon, AWS branch. Designed and coded web pages along with improving accessibility features for blind and visually impaired users.",
+  },
+];
+
+export default ExperienceData;
