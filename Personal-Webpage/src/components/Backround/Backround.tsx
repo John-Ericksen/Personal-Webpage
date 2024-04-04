18ade9dfcc4e50e50d06c3d0f4691b8ee76c5d07
John-Ericksen <105228679+John-Ericksen@users.noreply.github.com>
2024-04-04 18:43:03 -0400

    improve backround graphics

--- Personal-Webpage/src/components/Backround/Backround.tsx
+++ Personal-Webpage/src/components/Backround/Backround.tsx
@@ -3,7 +3,10 @@ import "./Backround.scss"
 export default function Backround() {
   return(
     <>
-      <div className="gradient gradient-home"/>
+      <div className="gradient-1"/>
+      <div className="gradient-2"/>
+      <div className="gradient-3"/>
+
       <div className="border"/>
     </>
     
