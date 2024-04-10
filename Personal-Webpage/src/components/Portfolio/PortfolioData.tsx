dd47a643abb318006b9f7668d14552b239fb5a64
John Ericksen <john.paul.ericksen@gmail.com>
2024-04-09 20:12:00 -0400

    change logos

--- Personal-Webpage/src/components/Portfolio/PortfolioData.tsx
+++ Personal-Webpage/src/components/Portfolio/PortfolioData.tsx
@@ -2,7 +2,7 @@ const TechPortfolioData = [
   {
     title: "Personal Webpage",
     image:
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s",
+    "/logo.svg",
     text: "This current site. Created using React, Typescript, and SASS, this web page hosts information about myself and the projects I've worked on.",
     link: "http://localhost:5173/",
   },
