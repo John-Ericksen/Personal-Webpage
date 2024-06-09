986aa6f3df2b7363616f7a1434eaa6fb04e46c0e
John Ericksen <john.paul.ericksen@gmail.com>
2024-06-09 16:58:01 -0700

    update data for skills and experience

--- src/components/Experience/ExperienceData.tsx
+++ src/components/Experience/ExperienceData.tsx
@@ -1,16 +1,16 @@
 const ExperienceData = [
   {
+    title: "QuattronKids",
+    subtitle: "Programming Instructor",
+    image: "https://www.quattronkids.com/logo.jpg",
+    text: "Instructed others in the fundamentals of programming, Python, and Game Development.",
+  },
+  {
     title: "Amazon",
     subtitle: "Software Engineering Intern",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s",
     text: "Interned at Amazon, AWS branch. Designed and coded web pages along with improving accessibility features for blind and visually impaired users.",
-  },
-  {
-    title: "QuattronKids",
-    subtitle: "Programming Instructor",
-    image: "https://www.quattronkids.com/logo.jpg",
-    text: "Instructed others in the fundamentals of programming, Python, and Game Development.",
   },
 ];
 
--- src/components/Skills/SkillsData.tsx
+++ src/components/Skills/SkillsData.tsx
@@ -27,8 +27,9 @@ const tools = [
 const certifications = [
   "Machine Learning with Python (IBM)",
   "Intro to Deep Neural Networks with Keras (IBM)",
+  "Introduction to Computer Vision and Image Processing (IBM)",
+  "Deep Neural Networks with PyTorch (IBM)",
 ];
-
 
 const SkillsData = [programmingTechnologies, tools, certifications];
 export default SkillsData;
