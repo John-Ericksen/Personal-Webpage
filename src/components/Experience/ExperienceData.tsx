87f8282a61cb8a929f14ba6a8d1ad3508611b98b
John Ericksen <john.paul.ericksen@gmail.com>
2025-08-25 15:51:56 -0400

    add freewheel experience

--- src/components/Experience/ExperienceData.tsx
+++ src/components/Experience/ExperienceData.tsx
@@ -1,16 +1,17 @@
 const ExperienceData = [
   {
-    title: "Stony Brook University",
+    title: "FreeWheel",
+    subtitle: "Software Engineering Intern",
+    image:
+      "https://builtin.com/sites/www.builtin.com/files/2022-08/Freewheel.jpg",
+    text: "Analyzed published computing journals and research papers to design and develop a new ad-space bidding system that reduces bid times by from 450ms to 10ms, resulting in less missed bids due to timeouts and more revenue generated off of won bids."
+  },
+  {
+    title: "Stony Brook",
     subtitle: "Website Development Intern",
     image:
       "https://www.stonybrook.edu/commcms/university-honors-programs/_images/red-shield-big.png",
     text: "Designed and implemented web pages for multiple Stony Brook University departments, identified and updated/removed outdated and broken links from Stony Brook University owned web pages, and created online contact forms on Stony Brook web pages resulting in over 150% more responses from web page users.",
-  },
-  {
-    title: "QuattronKids",
-    subtitle: "Programming Instructor",
-    image: "/quattronkdis_logo.png",
-    text: "Instructed hundreds of students on the fundamentals of programming such as variables, data types, and good coding practice. Taught the syntax of Python, and how to utilize Python to make software applications. Guided students through the process of game development, from concept to implementation.",
   },
   {
     title: "Amazon",
@@ -18,6 +19,12 @@ const ExperienceData = [
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s",
     text: "Interned at Amazon, AWS branch. Designed and coded web pages along with improving accessibility features for blind and visually impaired users, along with developed a link checking tool, ensuring the health and accuracy of links on AWS pages.",
+  },
+  {
+    title: "QuattronKids",
+    subtitle: "Programming Instructor",
+    image: "/quattronkdis_logo.png",
+    text: "Instructed hundreds of students on the fundamentals of programming such as variables, data types, and good coding practice. Taught the syntax of Python, and how to utilize Python to make software applications. Guided students through the process of game development, from concept to implementation.",
   },
 ];
 
