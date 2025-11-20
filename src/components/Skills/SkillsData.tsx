d827e909d0614e947435ccc1a76911caa1f64fb7
John Ericksen <john.paul.ericksen@gmail.com>
2025-11-20 17:38:43 -0500

    update text content

--- src/components/Skills/SkillsData.tsx
+++ src/components/Skills/SkillsData.tsx
@@ -5,41 +5,28 @@ const programmingTechnologies = [
   "Python",
   "Java",
   "TypeScript",
+  "JavaScript",
   "React",
-  "SASS",
   "HTML",
   "CSS",
-  "Machine Learning",
-  "JavaScript",
-  "C#",
-  "C",
 ];
 
 const toolsAndSkills = [
+  "Data Structures & Algorithms",
+  "Object-Oriented Design",
+  "Cloud Computing (AWS)",
+  "Algorithm Design",
+  "Human-Computer Interaction",
   "UI/UX Design",
   "Accessible Software Development",
-  "Asana",
-  "Figma",
-  "Photoshop",
   "Project Management",
-  "Machine Learning",
-  "Artificial Intelligence",
-  "Deep Neural Networks",
-  "Documentation Writing",
-  "Teamwork and Interpersonal Communication",
   "Testing (Unit, End-to-End, Integration)",
-  "Adaptability",
 ];
 
 const certifications = [
   "Department of Homeland Security Standards for Web",
   "Department of Homeland Security Testing Tools",
   "IBM AI Engineering Professional",
-  "Deep Learning Models with TensorFlow (IBM)",
-  "Deep Neural Networks with PyTorch (IBM)",
-  "Machine Learning with Python (IBM)",
-  "Intro to Deep Neural Networks with Keras (IBM)",
-  "Introduction to Computer Vision and Image Processing (IBM)",
 ];
 
 const SkillsData = [programmingTechnologies, toolsAndSkills, certifications];
